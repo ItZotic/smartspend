4f0150245927e45f1d984140bf150205e6b4dc76
ItZotic <jm.pimentel2101@gmail.com>
2025-10-30 17:26:27 +0800

    HOPEFULLY WORKING

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:smartspend/firebase_options.dart';
 import 'package:smartspend/login.dart';
 
-import 'firebase_options.dart';
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
